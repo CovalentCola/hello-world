8dd3b90e68859f646479d50517fb90cad4c6ed02
CovalentCola <59984012+CovalentCola@users.noreply.github.com>
2020-10-17 17:40:14 +0100

    Update Guess The Number.py

--- Guess The Number.py
+++ Guess The Number.py
@@ -1,19 +1,36 @@
 import random
+import sys
 
-x = random.randrange(1, 100, 1, int)
+def x_randomiser():
+    x = random.randrange(1, 100, 1, int)
+    return x
 
+def random_guesser ():
+    global x
+    user_guess = input("Input an integer between 1-100:\n")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+        if user_guess == x:
+            print("Well done! The answer was indeed %d!" % x)
+            rerun = input("Do you want to play again? <Y/N>\n")
+            if rerun == "Y":
+                x = x_randomiser()
+                random_guesser()
+            else:
+                sys.exit()
+        elif user_guess < x:
+            print("No, that's too low! Try again.")
+            random_guesser()
+        elif user_guess > x:
+            print("No, that's too high! Try again.")
+            random_guesser()
+        else:
+            print('Try again...\n')
+            random_guesser()
+    else:
+        print("I said an integer!\n")
+        random_guesser()
 
-def random_guesser (x):
-    user_guess = int(input("Input an integer between 1-100:\n"))
-    if user_guess == x:
-        print("Well done! The answer was indeed %d!" % x)
-        input("Please rerun the script to play again.\n")
-    elif user_guess < x:
-        print("No, that's too low! Try again.")
-        random_guesser(x)
-    elif user_guess > x:
-        print("No, that's too high! Try again.")
-        random_guesser(x)
-
-random_guesser(x)
+x = x_randomiser()
+random_guesser()
 
